db5a1c33ac8ddd6832dc0966bb7aac6887f49e7f
TyRobinson <ty.robinson@trustscience.com>
2019-02-01 20:20:28 -0700

    added Tweet class and fixed function from Lab 2

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter.models;
+
+import java.util.Date;
+
+public class Tweet {
+    String message;
+    Date date;
+
+    Tweet(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public getMessage()
+}
